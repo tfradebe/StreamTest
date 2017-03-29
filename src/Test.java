f1cb58bbe95a1a86f23b22dd6838d56dd64fe258
tefo-matshediso-tlotla <tfradebe@gmail.com>
2017-03-29 08:17:38 +0200

    map function method

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,11 @@ import java.util.stream.Stream;
 public class Test{
     public static void main(String... args){
         Stream<Integer> streamInteger = Stream.iterate(1, n -> n+1);
-        streamInteger.skip(5).limit(2).forEach(System.out::println);
+        Stream<Integer> newStream = streamInteger.skip(5).limit(5);
+        newStream.forEach(System.out::println);
+        Stream<String> streamString = Stream.of("monkey","gorilla","bonobo");
+        Stream<Integer> numStream = streamString.map(String::length);
+        numStream.forEach(System.out::println);
 
     }
 }
